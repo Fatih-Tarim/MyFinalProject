38b8c29e9fd0b8d4e7fea13c10d6a3e0f8c45496
Fatih TARIM <fatihtrm99@hotmail.com>
2021-02-06 23:28:40 +0300

    06.02.2021

---
+++ Entities/Concrete/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string CustomerId { get; set; }
+        public int EmployeeId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string ShipCity { get; set; }
+    }
+}
